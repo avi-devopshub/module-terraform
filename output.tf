fad34cbe16e51e3ab1a31ad8011e178319d3e88c
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-05 14:48:45 +0530

    Fix output value references for VPC and subnets in output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "vpc_id"{
-    value = module.vpc.my_vpc.id
+    value = module.vpc.my_vpc_id
 }
 output "public_subnet_id"{
-    value = module.vpc.public_subnet.id
+    value = module.vpc.public_subnet_id
 }
 output "private_subnet_id"{
-    value = module.vpc.private_subnet.id
+    value = module.vpc.private_subnet_id
 }
