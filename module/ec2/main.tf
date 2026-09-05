2351583cbe288efb21d1619a3ed945c21db9e11a
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-05 15:53:04 +0530

    Fix user_data file path syntax in EC2 instance resource

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "my_ec2"{
     vpc_id = var.vpc_id
     subnet_id = var.public_subnet_id
     vpc_security_group_ids = [var.sg_id]
-    user_data = filebase64(/root/module-terraform/module/ec2/user_data.sh)
+    user_data = filebase64("/root/module-terraform/module/ec2/user_data.sh")
 
 }
