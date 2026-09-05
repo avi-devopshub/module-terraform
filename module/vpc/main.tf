0ca4314c7dce7eda929983d873deb8b06789703b
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-05 14:21:53 +0530

    Add VPC module configuration and fix public subnet IP mapping

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -10,6 +10,7 @@ resource "aws_subnet" "public_subnet"{
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.public_subnet_cidr
     availability_zone = var.public_az
+     map_public_ip_on_launch = true s
     tags = {
         Name = var.public_subnet_name
     }
