aa574e6436fb0c5786efca20c382bbc9f2b9cbca
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-05 14:31:02 +0530

    Fix typo in public subnet resource configuration and add AWS provider setup

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_subnet"{
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.public_subnet_cidr
     availability_zone = var.public_az
-     map_public_ip_on_launch = true s
+     map_public_ip_on_launch = true
     tags = {
         Name = var.public_subnet_name
     }
