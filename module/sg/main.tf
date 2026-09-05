300b91f3c6984dfd5112df8e8b529eb53e666a11
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-05 15:30:13 +0530

    Add security group resource and outputs for security group ID

---
+++ module/sg/main.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "my_sg"{
+    name = var.sg_name
+    description = var.sg_name
+    vpc_id = var.vpc_id
+    ingress{
+        from_port = var.ssh_port
+        to_port = var.ssh_port
+        protocol = var.protocol
+        cidr_block = var.cidr
+    }
+    ingress{
+        from_port = var.http_port
+        to_port = var.http_port
+        protocol = var.protocol
+        cidr_block = var.cidr
+    }
+    egress{
+        from_port = var.egress_port
+        to_port = var.egress_port
+        protocol = var.egress_protocol
+        cidr_block = var.cidr
+    }
+
+}
---
+++ module/sg/var.tf
@@ -0,0 +1,9 @@
+variable "sg_name" {}
+variable "ssh_port" {}
+variable "protocol" {}
+variable "cidr" {}
+variable "http_port" {}
+variable "egress_port" {}
+variable "egress_protocol" {}
+
+
